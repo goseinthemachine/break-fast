be1b78d2a0eaca6fbfc23410d5e39a54bc61585a
Cameron <cameron.gose@hey.com>
2021-10-27 11:40:13 -0700

    feat: add attack states with timings

--- app/scenes/action_scene.rb
+++ app/scenes/action_scene.rb
@@ -23,7 +23,9 @@ class ActionScene
     outputs.labels  << [580, 460, inputs.controller_one.left_analog_y_perc, 5, 1]
     outputs.labels  << [640, 500, inputs.controller_one.right_analog_x_perc, 5, 1]
     outputs.labels  << [640, 460, inputs.controller_one.right_analog_y_perc, 5, 1]
-    outputs.labels  << [640, 420, "v:#{state.player.dx} state: #{state.player.state}", 5, 1]
+    outputs.labels  << [640, 420, "attack: #{state.player.attack_state} state: #{state.player.state}", 5, 1]
+    outputs.labels  << [640, 390, "jump elapsed time: #{state.player.jump.at ? state.player.jump.at.elapsed_time : ''}", 5, 1]
+    outputs.labels  << [640, 360, "attack elapsed time: #{state.player.attack.at ? state.player.attack.at.elapsed_time : ''}", 5, 1]
 
     render_background
 
@@ -48,78 +50,91 @@ class ActionScene
 
     outputs.sprites << player
 
-
-
-
-
   end
 
-  def handle_input
+  def handle_player_movement
     if inputs.controller_one.left_analog_x_perc > 0
       if state.player.dx < state.player_max_run_speed
         state.player.dx += if state.player.y > state.bridge_top
                              0.2 * inputs.controller_one.left_analog_x_perc
-        else
-          inputs.controller_one.left_analog_x_perc
+                           else
+                             inputs.controller_one.left_analog_x_perc
                            end
       end
     elsif inputs.controller_one.left_analog_x_perc < 0
       if state.player.dx > -state.player_max_run_speed
         state.player.dx += if state.player.y > state.bridge_top
                              0.2 * inputs.controller_one.left_analog_x_perc
-        else
-          inputs.controller_one.left_analog_x_perc
+                           else
+                             inputs.controller_one.left_analog_x_perc
                            end
       end
     else
       state.player.dx *= if state.player.y > state.bridge_top
                            state.air_friction
-      else
-        state.friction
+                         else
+                           state.friction
                          end
 
     end
+  end
+
+  def handle_input
+    if state.player.state != :attacking
+      handle_player_movement
+    end
+
+    if !state.player.falling
+      if inputs.controller_one.key_down.y
+        state.player.dy = 15
+        state.player.jump.at ||= state.tick_count
+      end
+    end
+
+    if inputs.controller_one.key_up.y
+      state.player.jump.at = nil
+    end
+    if inputs.controller_one.key_down.x
+      state.player.state = :attacking
+      state.player.attack.at ||= state.tick_count
+      if state.player.falling
+        state.player.attack_state = :nair
+      elsif  inputs.controller_one.left_analog_x_perc.abs > 0
+        state.player.attack_state = :fattack
+      else
+        state.player.attack_state = :nattack
+      end
 
 
-    if inputs.controller_one.key_held.y && state.player.y <= state.bridge_top
-      state.player.dy = 15
     end
   end
 
-  def calc
-    # Since velocity is the change in position, the change in x increases by dx. Same with y and dy.
-    if state.player.dx.abs < 0.1
-      state.player.state = :standing
+  def calc_attack
+    case state.player.attack_state
+    when :nattack
       state.player.dx = 0
-    else
-      state.player.state = :running
+      if state.player.attack.at.elapsed_time > state.player.attack.nattack_duration
+        state.player.state = :standing
+        state.player.attack.at = nil
+        state.player.attack_state = nil
+      end
+    when :nair
+      if state.player.attack.at.elapsed_time > state.player.attack.nair_duration
+        state.player.state = :jumping
+        state.player.attack.at = nil
+        state.player.attack_state = nil
+      end
+    when :fattack
+      if state.player.attack.at.elapsed_time > state.player.attack.fattack_duration
+        state.player.state = :standing
+        state.player.attack.at = nil
+        state.player.attack_state = nil
+      end
     end
+  end
 
-    # if state.player.y <= state.bridge_top
-    #   if state.player.dx < 0
-    #     state.player.r = 180
-    #   elsif state.player.dx > 0
-    #     state.player.r = 0
-    #   end
-    # end
-
-    if state.player.y > state.bridge_top
-      state.player.state = :jumping
-    end
-
-    state.player.x  += state.player.dx
-    state.player.y  += state.player.dy
-
-    # Since acceleration is the change in velocity, the change in y (dy) increases every frame
-    state.player.dy += state.gravity
-
-    # player's y position is either current y position or y position of top of
-    # bridge, whichever has a greater value
-    # ensures that the player never goes below the bridge
-    state.player.y  = state.player.y.greater(state.bridge_top)
-
-    # player's x position is either the current x position or 0, whichever has a greater value
-    # ensures that the player doesn't go too far left (out of the screen's scope)
+  def calc_x
+    state.player.x += state.player.dx
     state.player.x  = state.player.x.greater(0)
     if state.player.x >= state.bridge_end - state.player.w ||
       state.player.x <= 0
@@ -127,15 +142,45 @@ class ActionScene
     end
     state.player.x = state.player.x.lesser(state.bridge_end - state.player.w)
 
-    # player is not falling if it is located on the top of the bridge
-    state.player.falling = false if state.player.y == state.bridge_top
     #state.player.rect = [state.player.x, state.player.y, state.player.h, state.player.w] # sets definition for player
 
     if state.player.state == :running && state.player.dx.abs > 0
       state.player.flip_horizontally = state.player.dx <= 0
     end
+  end
 
-    # Update player sprite
+  def calc_y
+    if state.player.y > state.bridge_top && state.player.state != :attacking
+      state.player.state = :jumping
+    end
+    state.player.y  += state.player.dy
+    state.player.dy += state.gravity
+    state.player.y  = state.player.y.greater(state.bridge_top)
+    # player is not falling if it is located on the top of the bridge
+    state.player.falling = state.player.y > state.bridge_top
+  end
+
+  def calc
+    if state.player.state == :attacking && state.player.falling
+      calc_x
+      calc_attack
+    elsif state.player.state == :attacking
+      calc_attack
+    else
+      # Since velocity is the change in position, the change in x increases by dx. Same with y and dy.
+      if state.player.dx.abs < 0.1
+        state.player.state = :standing
+        state.player.dx = 0
+      else
+        state.player.state = :running
+      end
+
+      calc_x
+
+    end
+
+    calc_y
+
     state.player.frame =
       case(state.player.state)
       when :standing
@@ -149,7 +194,6 @@ class ActionScene
       else
         'sprites/frame0000.png'
       end
-
   end
 
   def defaults
@@ -194,6 +238,10 @@ class ActionScene
     state.player_max_run_speed        = 10
     state.player_speed_slowdown_rate  = 0.9
     state.player_acceleration         = 0.9
+    state.player.attack.nattack_duration = 5
+    state.player.attack.fattack_duration = 10
+    state.player.attack.nair_duration = 5
+    state.player.action.jump_duration = 5
   end
 
 end
